a902e20bbf06cd979ea8a663a5e8818253024cb3
sseidl88 <samueleseidl@gmail.com>
2020-03-28 15:32:36 -0500

    added fire and star particle effects

--- ParticleSystemStarter/Game1.cs
+++ ParticleSystemStarter/Game1.cs
@@ -20,8 +20,16 @@ namespace ParticleSystemStarter
         ParticleSystem particleSystem;
         ParticleSystem steam;
         Texture2D steamTexture;
+        //fire
+        ParticleSystem fire;
+        Texture2D fireTexture;
+        //star
+        ParticleSystem star;
+        Texture2D starTexture;
         Texture2D particleTexture;
         private Random random = new Random();
+        private Random randColor = new Random();
+        int colorPick;
         public Pan fryingPan = new Pan(270, 260);
         public int steamSpawnX;
         public Hamburger rawMeat = new Hamburger(600, 10);
@@ -30,10 +38,13 @@ namespace ParticleSystemStarter
         public Plate plate = new Plate(300, 90);
         public Buns bun = new Buns(10, 10);
         public Bell bell = new Bell(40, 350);
-        SpriteFont font; 
+        SpriteFont font;
+        public Trash trash = new Trash(700, 350);
 
         //score
         int score = 0;
+        bool complete = false;
+        public float starTimer = 0;
 
 
         public Game1()
@@ -71,9 +82,10 @@ namespace ParticleSystemStarter
             plate.LoadContent(Content);
             bun.LoadContent(Content);
             bell.LoadContent(Content);
+            trash.LoadContent(Content);
            // hand.LoadContent(Content);
             particleTexture = Content.Load<Texture2D>("particle");
-            particleSystem = new ParticleSystem(GraphicsDevice, 1000, particleTexture);
+            particleSystem = new ParticleSystem(GraphicsDevice, 500, particleTexture);
             particleSystem.Emitter = new Vector2(100, 100);
             particleSystem.SpawnPerFrame = 4;
             //steam
@@ -81,7 +93,16 @@ namespace ParticleSystemStarter
             steam = new ParticleSystem(GraphicsDevice, 100, steamTexture);
             steam.Emitter = new Vector2(100, 100);
             steam.SpawnPerFrame = 4;
-
+            //fire
+            fireTexture = Content.Load<Texture2D>("fireParticle");
+            fire = new ParticleSystem(GraphicsDevice, 1000, fireTexture);
+            fire.Emitter = new Vector2(100, 100);
+            fire.SpawnPerFrame = 10;
+            //star
+            starTexture = Content.Load<Texture2D>("star");
+            star = new ParticleSystem(GraphicsDevice, 5, starTexture);
+            star.Emitter = new Vector2(100, 100);
+            star.SpawnPerFrame = 1;
             // Set the SpawnParticle method
             particleSystem.SpawnParticle = (ref Particle particle) =>
             {
@@ -127,7 +148,53 @@ namespace ParticleSystemStarter
 
             };
 
+            //fire
+            fire.SpawnParticle = (ref Particle fireParticle) =>
+            {
+                steamSpawnX = (int)random.Next(300, 500);
+                fireParticle.Position = new Vector2(steamSpawnX, fryingPan.position.Y + 50);
+                fireParticle.Velocity = new Vector2(
+                    MathHelper.Lerp(-50, 50, (float)random.NextDouble()), // X between -50 and 50
+                    MathHelper.Lerp(0, 100, (float)random.NextDouble()) // Y between 0 and 100
+                    );
+                fireParticle.Acceleration = 0.1f * new Vector2(0, (float)-random.NextDouble());
 
+               // fireParticle.Color = Color.DarkRed;
+                fireParticle.Scale = 1f;
+                fireParticle.Life = 15;
+                colorPick = randColor.Next(1, 4);
+
+                
+                if (colorPick == 1)
+                    fireParticle.Color = Color.YellowGreen;
+                else if (colorPick == 2)
+                    fireParticle.Color = Color.MonoGameOrange;
+                else
+                    fireParticle.Color = Color.Red;
+                
+
+                    
+
+            };
+
+            //star
+            star.SpawnParticle = (ref Particle particle) =>
+            {
+               int starSpawnX = (int)random.Next((int)bell.position.X, (int)(bell.position.X + 70));
+                particle.Position = new Vector2(starSpawnX, bell.position.Y - 20 );
+                particle.Velocity = new Vector2(
+                    MathHelper.Lerp(-50, 50, (float)random.NextDouble()), // X between -50 and 50
+                    MathHelper.Lerp(0, 100, (float)random.NextDouble()) // Y between 0 and 100
+                    );
+                // particle.Velocity = new Vector2(1,1);
+                particle.Acceleration = 0.1f * new Vector2(0, (float)-random.NextDouble());
+                particle.Color = Color.Aquamarine;
+                particle.Scale = 2;
+                 particle.Life = 3.0f;
+                //particle.Life = 200;
+
+
+            };
 
             // Set the UpdateParticle method
             particleSystem.UpdateParticle = (float deltaT, ref Particle particle) =>
@@ -143,6 +210,32 @@ namespace ParticleSystemStarter
                 particle.Velocity += deltaT * particle.Acceleration;
                 particle.Position -= deltaT * particle.Velocity;
                 particle.Scale -= deltaT;
+                particle.Life -= deltaT;
+            };
+            //fire update
+            fire.UpdateParticle = (float deltaT, ref Particle fireParticle) =>
+            {
+                fireParticle.Velocity += deltaT * fireParticle.Acceleration;
+                fireParticle.Position -= deltaT * fireParticle.Velocity;
+                fireParticle.Scale -= deltaT;
+                fireParticle.Life -= deltaT;
+                //change color
+                //fireParticle.Color = Color.Yellow;
+                //if (fireParticle.Life > 15)
+                //    fireParticle.Color = Color.Yellow;
+                //else if (fireParticle.Life > 10 && fireParticle.Life < 15)
+                //    fireParticle.Color = Color.Orange;
+                //else if (fireParticle.Life > 5 && fireParticle.Life < 10)
+                //    fireParticle.Color = Color.Red;
+                //else
+                //    fireParticle.Color = Color.TransparentBlack;
+            };
+            //star update
+            star.UpdateParticle = (float deltaT, ref Particle particle) =>
+            {
+                particle.Velocity += deltaT * particle.Acceleration;
+                particle.Position -= deltaT * particle.Velocity;
+              //  particle.Scale -= deltaT;
                 particle.Life -= deltaT;
             };
 
@@ -171,7 +264,11 @@ namespace ParticleSystemStarter
             // TODO: Add your update logic here
             particleSystem.Update(gameTime);
             steam.Update(gameTime);
+            fire.Update(gameTime);
+            star.Update(gameTime);
+
             fryingPan.Update(gameTime);
+
 
             //picking up raw meat
             if (hand.RectBounds.Intersects(rawMeat.RectBounds) && 
@@ -241,9 +338,19 @@ namespace ParticleSystemStarter
                 if(plate.animateState == PlateAnimation.burger)
                 {
                     score += 10;
+                    complete = true;
+                    starTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
                 }
                 plate.animateState = PlateAnimation.empty;
             }
+            //picking up burnt burger
+            //if (hand.RectBounds.Intersects(fryingPan.RectBounds) &&
+            //hand.animateState == handAnimation.closed && handContent == handCarry.empty &&
+            //fryingPan.animateState == PanAnimation.burnt)
+            //{
+            //    handContent = handCarry.burntBurger;
+            //    fryingPan.animateState = PanAnimation.empty;
+            //}
 
             base.Update(gameTime);
         }
@@ -265,14 +372,29 @@ namespace ParticleSystemStarter
             spriteBatch.DrawString(font, "Score: " + score.ToString(), new Vector2(350, 20), Color.White);
 
             fryingPan.Draw(spriteBatch);
+
             //steam
             if(fryingPan.animateState == PanAnimation.raw || fryingPan.animateState == PanAnimation.done)
             steam.Draw();
+            //fire
+            if(fryingPan.animateState == PanAnimation.burnt)
+            fire.Draw();
+            //star
+            while (complete && starTimer < 2)
+            {
+                star.Draw();
+                complete = false;
+            }
+            starTimer = 0;
+
+
             rawMeat.Draw(spriteBatch);
             plate.Draw(spriteBatch);
             bun.Draw(spriteBatch);
             bell.Draw(spriteBatch);
-            //draw hand last so it goes ver everything else
+            //trash doesn't work currently
+            //trash.Draw(spriteBatch);
+            //draw hand last so it goes over everything else
             hand.Draw(spriteBatch);
 
             spriteBatch.End();
---
+++ ParticleSystemStarter/Trash.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameWindowsStarter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSystemStarter
+{
+    public class Trash
+    {
+
+        public BoundingRectangle position;
+        public Texture2D trash;
+        ContentManager content;
+
+        public Rectangle RectBounds
+        {
+            get { return position; }
+        }
+
+
+        public Trash(int x, int y)
+        {
+            position.X = x;
+            position.Y = y;
+            position.Height = 160;
+            position.Width = 160;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            this.content = content;
+            trash = this.content.Load<Texture2D>("trash");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(trash, new Vector2(position.X, position.Y), Color.White);
+        }
+    }
+}
